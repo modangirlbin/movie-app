55c054a379b084068e8ad9b1bccafa628dd40f54
poohinajar@gmail.com <poohinajar@gmail.com>
2022-08-18 19:42:16 +0900

    메인이미지 임시 수정, 토글메뉴 추기

--- src/component/views/Header/Header.js
+++ src/component/views/Header/Header.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../../../scss/import.scss';
 // import logo from '../../../../img/logo.png';
 
 function Header(props) {
-  // const [isOpen, setMenu] = useState(false);
+  const [isOpen, setMenu] = useState(false);
   
-  // const toggleMenu = () => {
-  //   console.log('hi');
-  //       // setMenu(isOpen => !isOpen); // on,off 개념 boolean
-  //   }
-  //   const hiHello = () => {
-  //     console.log('hi')
-  //   }
+  const toggleMenu = (e) => {
+    setMenu(isOpen => !isOpen); // on,off
+    console.log(e.target);
+    e.target.Attr('aria-haspopup', 'false')
+  }
+
   return (
     <header id='header' className='header'>
       <h1 className='title_logo'>
         <a href='#none' alt='메가박스 홈'>
-          {/* <img src={logo} alt='' /> */}
           <img src='https://img.megabox.co.kr/static/mb/images/common/ci/logo-white_new.png' alt='' />
-          {/* Movie App */}
         </a>
       </h1>
       <a href='#none' className='link_ticket'>
         <span className='offscreen'>내 예매권 보기</span>
       </a>
-      <button className='btn_gnb' type='button' aria-haspopup='true' aria-expanded='false' aria-controls='gnb'><span className="offscreen">메인 메뉴</span></button>
-      <nav id='gnb' className='gnb'>
+      <button className='btn_gnb' type='button' aria-haspopup={isOpen} aria-expanded={isOpen} aria-controls='gnb' onClick={(e)=>toggleMenu(e)}><span className="offscreen">메인 메뉴</span></button>
+      <nav className={isOpen ? "gnb on" : "gnb"}>
         <h2 className='offscreen'>메인메뉴</h2>
         <ul className='list_gnb'>
           <li className='item_gnb'><a href="#none">예매하기</a></li>
