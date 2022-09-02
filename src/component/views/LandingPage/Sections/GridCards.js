78731aeff46258c0c154d598413bd420b53a38d0
poohinajar@gmail.com <poohinajar@gmail.com>
2022-09-02 20:52:22 +0900

    서브페이지 이동 주석처리

--- src/component/views/LandingPage/Sections/GridCards.js
+++ src/component/views/LandingPage/Sections/GridCards.js
@@ -4,7 +4,8 @@ import '../../../../scss/import.scss';
 function GridCards(props) {
   return (
     <li className='item_card'>
-      <a href={`/movie/${props.movieId}`}>
+      {/* <a href={`/movie/${props.movieId}`}> */}
+      <a href="#none">
         <span className='inner_card'>
           <img src={props.image} className='thumb_card' alt={props.movieName} />
           <span className='vote_card'><span className='offscreen'>평점</span>{props.movieVote}</span>
