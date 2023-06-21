0b0cd28343364e0fb5a50ae91ed8c83b3189fd7b
poohinajar@gmail.com <poohinajar@gmail.com>
2023-06-21 16:23:48 +0900

    깃페이지 browerouter 제공하지 않아 hashrouter로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Footer from './component/views/Footer/Footer';
 import LandingPage from './component/views/LandingPage/LandingPage';
@@ -6,14 +6,14 @@ import DetailPage from './component/views/DetailPage/DetailPage';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           {/* <Route path="/movie-app/" element={<DetailPage />} /> */}
           <Route path="/:movieId" element={<DetailPage />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
