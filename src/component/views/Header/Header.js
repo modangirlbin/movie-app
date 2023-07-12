d567c5b6830b6b12cc22e2557dc9bdca0e9d6737
poohinajar@gmail.com <poohinajar@gmail.com>
2023-07-12 15:23:31 +0900

    메뉴 및 헤더 페이지 없는 링크 이동 막기

--- src/component/views/Header/Header.js
+++ src/component/views/Header/Header.js
@@ -10,24 +10,28 @@ function Header(props) {
     console.log(e.target);
     e.target.Attr('aria-haspopup', 'false')
   }
+  
+  function preventAnchor(e) {
+    e.preventDefault();
+  }
 
   return (
     <header id='header' className='header'>
       <h1 className='title_logo'>
-        <a href='#none'>
+        <a href='#/'>
           <img src='https://img.megabox.co.kr/static/mb/images/common/ci/logo-white_new.png' alt='메가박스 홈' />
         </a>
       </h1>
-      <a href='#none' className='link_ticket'>
+      <a href='#none' onClick={preventAnchor} className='link_ticket'>
         <span className='offscreen'>내 예매권 보기</span>
       </a>
       <button className='btn_gnb' type='button' aria-haspopup={isOpen} aria-expanded={isOpen} aria-controls='gnb' onClick={(e)=>toggleMenu(e)}><span className="offscreen">메인 메뉴</span></button>
       <nav id='gnb' className={isOpen ? "gnb on" : "gnb"}>
         <h2 className='offscreen'>메인메뉴</h2>
         <ul className='list_gnb'>
-          <li className='item_gnb'><a href="#none">예매하기</a></li>
-          <li className='item_gnb'><a href="#none">박스오피스</a></li>
-          <li className='item_gnb'><a href="#none">이벤트</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>예매하기</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>박스오피스</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>이벤트</a></li>
         </ul>
       </nav>
     </header>
--- src/component/views/Header/HeaderSub.js
+++ src/component/views/Header/HeaderSub.js
@@ -1,6 +1,5 @@
 import React,{useState} from 'react';
 import '../../../scss/import.scss';
-// import logo from '../../../../img/logo.png';
 
 function HeaderSub(props) {
   const [isOpen, setMenu] = useState(false);
@@ -11,21 +10,25 @@ function HeaderSub(props) {
     e.target.Attr('aria-haspopup', 'false')
   }
 
+  function preventAnchor(e) {
+    e.preventDefault();
+  }
+
   return (
     <header id='header' className='header'>
       <h1 className='title_sub'>
         {props.title}
       </h1>
-      <a href='/movie-app' className='link_back'>
+      <a href='#/' className='link_back'>
         <span className='offscreen'>뒤로 가기</span>
       </a>
       <button className='btn_gnb' type='button' aria-haspopup={isOpen} aria-expanded={isOpen} aria-controls='gnb' onClick={(e)=>toggleMenu(e)}><span className="offscreen">메인 메뉴</span></button>
       <nav id='gnb' className={isOpen ? "gnb on" : "gnb"}>
         <h2 className='offscreen'>메인메뉴</h2>
         <ul className='list_gnb'>
-          <li className='item_gnb'><a href="#none">예매하기</a></li>
-          <li className='item_gnb'><a href="#none">박스오피스</a></li>
-          <li className='item_gnb'><a href="#none">이벤트</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>예매하기</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>박스오피스</a></li>
+          <li className='item_gnb'><a href="#none" onClick={preventAnchor}>이벤트</a></li>
         </ul>
       </nav>
     </header>
