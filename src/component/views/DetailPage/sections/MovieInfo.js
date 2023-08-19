81354d47e4dca292f523450fa6174b5706aa5e69
poohinajar@gmail.com <poohinajar@gmail.com>
2023-08-20 04:20:48 +0900

    상세페이지 영화정보 박스 정렬맞춤, 네이밍 정리

--- src/component/views/DetailPage/sections/MovieInfo.js
+++ src/component/views/DetailPage/sections/MovieInfo.js
@@ -4,22 +4,24 @@ function MovieInfo(props) {
   return (
     <>
       <div className="sub_visual" style={{backgroundImage: `url(${props.backdrop})`}}></div>
-      <article className='sub_cont'>
-        <div className="area_poster">
-          <div className="thumb_sub">
-            <img className="thumb_img" src={props.image} alt={props.title} />
+      <article className='sub_contents'>
+        <div className="area_summary">
+          <div className="thumb_summary">
+            <img className="img" src={props.image} alt={props.title} />
           </div>
-          <div className="info_visual">
-            <h2 className="tit_thumb">{props.title}</h2>
+          <div className="info_summary">
+            <h2 className="tit">{props.title}</h2>
             <span className='tit_original'>{props.original_title}</span>
             <span>{props.overview}</span>
             <span className='date'>{props.release_date} 개봉</span>
-            <span>{props.runtime}분</span>
-            <span>평점: {props.vote_average} / 투표수: {props.vote_count}</span>
+            <span className='time'>{props.runtime}분</span>
+            <span className='rate'>평점: {props.vote_average} / 투표수: {props.vote_count}</span>
           </div>
         </div>
-        <strong className='copy'>{props.tagline}</strong>
-        <p className='overview'>{props.desc}</p>
+        <div className='area_detail'>
+          <strong className='copy_detail'>{props.tagline}</strong>
+          <p className='story_detail'>{props.desc}</p>
+        </div>
       </article>
     </>
   );
